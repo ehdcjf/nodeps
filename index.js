de29754b45f4fe13552a49b01326bf02b33c37bf
ehdcjf <kdch0823@naver.com>
2025-04-29 08:32:39 +0900

    temp

--- index.js
+++ index.js
@@ -1,27 +1,17 @@
-// https://www.acmicpc.net/problem/31458
-// !!초콜릿 중독 주의!!
+https://github.com/ehdcjf/nodeps.git
 
-const 입력값 = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n');
-const 정답 = [];
-for (let 반복 = 1; 반복 < input.length; 반복++) {
-	const 스택 = [];
-	const 문자열 = 입력값[반복];
-	for (let 문자인덱스 = 0; 문자인덱스 < 문자열.length; 문자인덱스++) {
-		if (스택.length == 0) {
-			스택.push(문자열[문자인덱스]);
-		} else {
-			if (스택[스택.length - 1] == '!') {
-				스택.push(문자열[문자인덱스]);
-			} else {
-				스택.pop();
-				스택.push('1');
-			}
+
+const [N, ...rawData] = require('fs')
+	.readFileSync('./dev/stdin').toString().split('\n')
+const [r, c] = N.split(' ').map(Number)
+const graph = rawData.map(v => v.trimEnd().split(''))
+const start = { r: 0, y: 0 }
+for (let i = 0; i < r; i++) {
+	for (let j = 0; j < c; j++) {
+		if (graph[i][j] == 'I') {
+			start.r = i;
+			start.c = j;
 		}
 	}
-	const 마지막정수 = 스택.pop();
-	const 스택길이 = 스택.length;
-	if (스택길이 % 2 == 0) 정답.push(마지막정수);
-	else 정답.push(마지막정수 == '0' ? '1' : '0');
 }
-
-console.log(정답.join('\n'));
+const visited = Array.from(new Array(r), () => Array(c).fill(false))
