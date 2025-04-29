a53f113a134dabb9e2cab428462e04233baa9f3e
ehdcjf <kdch0823@naver.com>
2025-04-29 10:30:33 +0900

    30804 푸는중

--- index.ts
+++ index.ts
@@ -1,16 +1,30 @@
-// https://www.acmicpc.net/problem/31458
-// !!초콜릿 중독 주의!!
-/*입력
-첫 번째 줄에는 수식의 개수 $T$가 주어진다.
-$(1\le T\le 1\, 000)$
+const [[N], S] = require('fs')
+	.readFileSync('./dev/stdin').toString().split('\n').map((v:string)=>v.split(" ").map(Number))
+
+console.log(N,S)
+
+let start = 0;
+let end = 0;
+let fruits = [0,0,0,0,0,0,0,0,0]
 
 
-두 번째 줄부터 $T$개의 수식이 한 줄에 하나씩 주어진다.
-하나의 수식은 $a$개의 느낌표, 정수 $n$, $b$개의 느낌표가 공백 없이 순서대로 합쳐진 형태이다.
-$(0\le a,b\le 30;$ $0\le n\le 1)$
+while(S[start]==S[end]){
+    start+=1
+    fruits[S[start]-1]+=1
+}
+let maxLength =  start-end+1
 
-출력
-각 수식을 계산한 결과를 한 줄에 하나씩 출력한다.
+while(start<N){
+    while(fruits.filter(v=>v>0).length<=2){
 
-*/const input: string[] = require('fs').readFileSync('./dev/stdin').toString().trim().split('\n')
-const N = input.shift();
+    }
+    while(end<start){
+
+    }
+}
+
+
+
+
+
+
