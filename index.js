b79566185bbec1a9200e3d64fa0bdb5d3609a8b7
ehdcjf <kdch0823@naver.com>
2025-06-17 08:19:01 +0900

    🚧13334 작성중

--- index.js
+++ index.js
@@ -1,17 +1,272 @@
-https://github.com/ehdcjf/nodeps.git
+// 13334
 
+class PriorityQueue {
+	constructor(priority) {
+		this.heap = [];
+		this.pairIsInCorrectOrder = priority;
+	}
 
-const [N, ...rawData] = require('fs')
-	.readFileSync('./dev/stdin').toString().split('\n')
-const [r, c] = N.split(' ').map(Number)
-const graph = rawData.map(v => v.trimEnd().split(''))
-const start = { r: 0, y: 0 }
-for (let i = 0; i < r; i++) {
-	for (let j = 0; j < c; j++) {
-		if (graph[i][j] == 'I') {
-			start.r = i;
-			start.c = j;
+	getLeftChildIndex(parentIndex) {
+		return 2 * parentIndex + 1;
+	}
+
+	getRightChildIndex(parentIndex) {
+		return 2 * parentIndex + 2;
+	}
+
+	getParentIndex(childIndex) {
+		return Math.floor((childIndex - 1) / 2);
+	}
+
+	hasParent(childIndex) {
+		return this.getParentIndex(childIndex) >= 0;
+	}
+
+	hasLeftChild(parentIndex) {
+		return this.getLeftChildIndex(parentIndex) < this.heap.length;
+	}
+
+	hasRightChild(parentIndex) {
+		return this.getRightChildIndex(parentIndex) < this.heap.length;
+	}
+
+	leftChild(parentIndex) {
+		return this.heap[this.getLeftChildIndex(parentIndex)];
+	}
+
+	rightChild(parentIndex) {
+		return this.heap[this.getRightChildIndex(parentIndex)];
+	}
+
+	parent(childIndex) {
+		return this.heap[this.getParentIndex(childIndex)];
+	}
+
+	swap(indexA, indexB) {
+		const tmp = this.heap[indexA];
+		this.heap[indexA] = this.heap[indexB];
+		this.heap[indexB] = tmp;
+	}
+
+	peek() {
+		return this.heap.length == 0 ? null : this.heap[0];
+	}
+
+	isEmpty() {
+		return !this.heap.length;
+	}
+
+	pop() {
+		if (this.heap.length == 0) {
+			return null;
+		}
+
+		if (this.heap.length == 1) {
+			return this.heap.pop();
+		}
+
+		const item = this.heap[0];
+
+		this.heap[0] = this.heap.pop();
+		this.bubbleDown();
+		return item;
+	}
+
+	push(item) {
+		this.heap.push(item);
+		this.bubbleUp();
+		return this;
+	}
+
+	bubbleUp() {
+		let currentIndex = this.heap.length - 1;
+
+		while (
+			this.hasParent(currentIndex) &&
+			!this.pairIsInCorrectOrder(this.parent(currentIndex), this.heap[currentIndex])
+		) {
+			this.swap(currentIndex, this.getParentIndex(currentIndex));
+			currentIndex = this.getParentIndex(currentIndex);
+		}
+	}
+
+	bubbleDown() {
+		let currentIndex = 0;
+		let nextIndex = null;
+
+		while (this.hasLeftChild(currentIndex)) {
+			if (
+				this.hasRightChild(currentIndex) &&
+				this.pairIsInCorrectOrder(this.rightChild(currentIndex), this.leftChild(currentIndex))
+			) {
+				nextIndex = this.getRightChildIndex(currentIndex);
+			} else {
+				nextIndex = this.getLeftChildIndex(currentIndex);
+			}
+			if (this.pairIsInCorrectOrder(this.heap[currentIndex], this.heap[nextIndex])) {
+				break;
+			}
+			this.swap(currentIndex, nextIndex);
+			currentIndex = nextIndex;
 		}
 	}
 }
-const visited = Array.from(new Array(r), () => Array(c).fill(false))
+
+class MinHeap {
+	constructor() {
+		this.heap = [];
+	}
+
+	getLeftChildIndex(parentIndex) {
+		return 2 * parentIndex + 1;
+	}
+
+	getRightChildIndex(parentIndex) {
+		return 2 * parentIndex + 2;
+	}
+
+	getParentIndex(childIndex) {
+		return Math.floor((childIndex - 1) / 2);
+	}
+
+	hasParent(childIndex) {
+		return this.getParentIndex(childIndex) >= 0;
+	}
+
+	hasLeftChild(parentIndex) {
+		return this.getLeftChildIndex(parentIndex) < this.heap.length;
+	}
+
+	hasRightChild(parentIndex) {
+		return this.getRightChildIndex(parentIndex) < this.heap.length;
+	}
+
+	leftChild(parentIndex) {
+		return this.heap[this.getLeftChildIndex(parentIndex)];
+	}
+
+	rightChild(parentIndex) {
+		return this.heap[this.getRightChildIndex(parentIndex)];
+	}
+
+	parent(childIndex) {
+		return this.heap[this.getParentIndex(childIndex)];
+	}
+
+	swap(indexA, indexB) {
+		const tmp = this.heap[indexA];
+		this.heap[indexA] = this.heap[indexB];
+		this.heap[indexB] = tmp;
+	}
+
+	peek() {
+		return this.heap.length == 0 ? null : this.heap[0];
+	}
+
+	isEmpty() {
+		return !this.heap.length;
+	}
+
+	pop() {
+		if (this.heap.length == 0) {
+			return null;
+		}
+
+		if (this.heap.length == 1) {
+			return this.heap.pop();
+		}
+
+		const item = this.heap[0];
+
+		this.heap[0] = this.heap.pop();
+		this.bubbleDown();
+		return item;
+	}
+
+	push(item) {
+		this.heap.push(item);
+		this.bubbleUp();
+		return this;
+	}
+
+	bubbleUp() {
+		let currentIndex = this.heap.length - 1;
+
+		while (
+			this.hasParent(currentIndex) &&
+			!this.pairIsInCorrectOrder(this.parent(currentIndex), this.heap[currentIndex])
+		) {
+			this.swap(currentIndex, this.getParentIndex(currentIndex));
+			currentIndex = this.getParentIndex(currentIndex);
+		}
+	}
+
+	bubbleDown() {
+		let currentIndex = 0;
+		let nextIndex = null;
+
+		while (this.hasLeftChild(currentIndex)) {
+			if (
+				this.hasRightChild(currentIndex) &&
+				this.pairIsInCorrectOrder(this.rightChild(currentIndex), this.leftChild(currentIndex))
+			) {
+				nextIndex = this.getRightChildIndex(currentIndex);
+			} else {
+				nextIndex = this.getLeftChildIndex(currentIndex);
+			}
+			if (this.pairIsInCorrectOrder(this.heap[currentIndex], this.heap[nextIndex])) {
+				break;
+			}
+			this.swap(currentIndex, nextIndex);
+			currentIndex = nextIndex;
+		}
+	}
+
+	pairIsInCorrectOrder(elementA, elementB) {
+		return elementA < elementB;
+	}
+}
+
+
+
+const priority = (a, b) => {
+	if (a.o < b.o) return true;
+	else if (a.o > b.o) return false;
+	else {
+		if (a.h < b.h) return true;
+		else return false;
+	}
+};
+
+
+
+const x = new PriorityQueue(priority)
+
+
+
+
+
+x.push({ h: 0, o: 100 })
+x.push({ h: 1, o: 100 })
+x.push({ h: 2, o: 100 })
+x.push({ h: 0, o: 101 })
+x.push({ h: 0, o: 102 })
+x.push({ h: 1, o: 102 })
+x.push({ h: 2, o: 102 })
+x.push({ h: 3, o: 102 })
+
+console.log(x.peek())
+while (!x.isEmpty()) {
+	console.log(x.pop())
+}
+
+
+
+
+
+
+const [[N], ...rawData] = require('fs')
+	.readFileSync('./dev/stdin').toString().split('\n').map(v => v.split(' ').map(Number))
+
+console.log(N)
+console.log(rawData)
